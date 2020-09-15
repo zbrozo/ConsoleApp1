e76b807b407f604f3816b328f3a4518c7afadcb5
Tomasz Zbrożek <tzbrozek@future-processing.com>
2020-09-15 09:33:56 +0200

    Program.cs and csproj added

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Thread t = new Thread(func);
+            //t.Start();
+
+            Task.Run(func);
+
+            Console.WriteLine("Hello World!");
+            //Console.ReadKey();
+        }
+
+        static async void Go()
+        {
+            await func();
+        }
+
+        static int func()
+        {
+            for (int i = 1; i < 9999; ++i)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(i);
+            }
+
+            return 0;
+        }
+
+    }
+}
